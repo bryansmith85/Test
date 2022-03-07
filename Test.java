8747b17ab9b2d199445540a676819b37e94289c4
bryansmith85 <101148027+bryansmith85@users.noreply.github.com>
2022-03-07 18:58:16 -0500

    Second Commit Test

--- Test.java
+++ Test.java
@@ -5,5 +5,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("This is a test!");
+        System.out.println("If this were a test, there would be another line!");
     }
 }
